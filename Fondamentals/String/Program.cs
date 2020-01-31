81eea715633f5bdf3337fcd6c25f2bdbb762b19b
éZekira <zekiramabanga@gmail.com>
2020-01-31 08:10:21 +0100

    suite

---
+++ Fondamentals/String/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int count = 1, len = 0;
+            Console.WriteLine("Enter a string: ");
+            str = Console.ReadLine();
+            while (len<=str.Length-1)
+            {
+                if (str[len]==' ' || str[len]=='\n' || str[len]=='\t')
+                {
+                    count++;
+                }
+                len++;
+            }
+            Console.WriteLine($"Total worsds : {count}");
+            int countSpace = 0;
+            foreach (char item in str)
+            {
+                if (item== ' ')
+                {
+                    countSpace++;
+                }
+            }
+            Console.WriteLine($"Total spaces : {countSpace}");
+
+            RemoveStringWhiteSpacedemo.RemoveSpaceUsingForEach();
+
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Fondamentals/String/RemoveStringWhiteSpace0demo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    public class RemoveStringWhiteSpacedemo
+    {
+        public static void RemoveSpaceUsingForEach()
+        {
+            const string str = "csharp is a programming language";
+            var chars = str.ToCharArray();
+            var str1 = string.Empty;
+            foreach (var eachChar in chars)
+            {
+                if (eachChar != ' ')
+                {
+                    str1 += eachChar;
+                }
+            }
+            Console.WriteLine(str1);
+        }
+    }
+}
